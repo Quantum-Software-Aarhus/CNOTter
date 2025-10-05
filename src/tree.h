7ea09e8285ae198cc171f2761acb55289b9358c1
Jaco van de Pol <jaco@cs.au.dk>
2025-10-05 21:26:52 +0200

    Clean up tree-compression code

--- src/tree.h
+++ src/tree.h
@@ -9,45 +9,62 @@ using leafset = HashSet<uint32_t, Linear, MurmurHash>;
 pairset intermediate;
 leafset leaves;
 
-// We need to fold NR leaves into one root
+// We need to fold NR leaves (uint64_t) into one uint64_t root
+// We build a tree as follows:
+// - The leaves are stored in idxs [0,NR)
+// - The intermediate nodes are stored in idxs [NR,2*NR-2)
+// - The root is not stored
+
+// Test if vetctor x is in the table.
+// Early termination if a subvector of x is unknown. 
 bool CONTAINS(Matrix x, rootset &table) {
-    uint32_t leave_idx[2*NR-1];
+    uint32_t idxs[2*NR-2];
 
-    for (uint8_t i=0; i<NR; i++)
-        if (!(leave_idx[i] = leaves.contains(x._bits[i]))) 
-            return false;
-    for (uint8_t i=NR, j=0; i<2*NR-2; i++, j+=2)
-        if (!(leave_idx[i] = intermediate.contains((uint64_t)x._bits[j]<<32 | x._bits[j+1]))) 
-            return false;
+    // Check if leaf indices are known and store them
+    for (uint8_t i=0; i<NR; i++) {
+        idxs[i] = leaves.contains(x._bits[i]);
+        if (!(idxs[i])) return false;
+    }
 
-    uint64_t root = (uint64_t)leave_idx[2*NR-2]<<32 | leave_idx[2*NR-1];
+    // Check if intermediate indices are known and store them
+    // node NR+i has children 2*i and 2*i+1
+    for (uint8_t i=0; i<NR-2; i++) {
+        uint64_t pair = (uint64_t)idxs[i*2]<<32 | idxs[i*2+1];
+        idxs[NR+i] = intermediate.contains(pair);
+        if (!(idxs[NR+i])) return false;
+    }
+    uint64_t root = (uint64_t)idxs[2*NR-4]<<32 | idxs[2*NR-3];
     return table.contains(root);
 }
 
 bool INSERT(Matrix x, rootset &table) {
-    uint32_t leave_idx[2*NR-1];
+    uint32_t idxs[2*NR-2];
 
+    // insert the leaves
     for (uint8_t i=0; i<NR; i++)
-        leave_idx[i] = leaves.findOrPut(x._bits[i]);
+        idxs[i] = leaves.findOrPut(x._bits[i]);
 
-    for (uint8_t i=NR, j=0; i<2*NR-2; i++, j+=2)
-        leave_idx[i] = intermediate.findOrPut((uint64_t)x._bits[j]<<32 | x._bits[j+1]);
+    // insert intermediate nodes
+    for (uint8_t i=NR; i<2*NR-2; i++) {
+        uint64_t pair = (uint64_t)idxs[i*2]<<32 | idxs[i*2+1];
+        idxs[NR+i] = intermediate.findOrPut(pair);
+    }
 
-    return table.insert((uint64_t)leave_idx[2*NR-2]<<32 | leave_idx[2*NR-1]);
+    return table.insert((uint64_t)idxs[2*NR-4]<<32 | idxs[2*NR-3]);
 }
 
 Matrix GET(uint64_t root) {
-    uint32_t leave_idx[2*NR-1];
-    leave_idx[2*NR-1] = root >> 32;
-    leave_idx[2*NR-2] = root;
-    for (uint8_t i=2*NR-1, j=2*NR-3; i>=NR; i--, j-=2) {
-        uint64_t next = intermediate.get(leave_idx[i]);
-        leave_idx[j] = intermediate.get(next >> 32);
-        leave_idx[j-1] = intermediate.get(next);
+    uint32_t idxs[2*NR-2];
+    idxs[2*NR-3] = root;
+    idxs[2*NR-4] = root >> 32;
+    for (uint8_t i=NR-3; i>=0; i--) {
+        uint64_t next = intermediate.get(idxs[NR+i]);
+        idxs[2*i+1] = intermediate.get(next);
+        idxs[2*i] = intermediate.get(next >> 32);
     }
     Matrix result;
     for (uint8_t i=0; i<NR; i++) {
-        result._bits[i] = leaves.get(leave_idx[i]);
+        result._bits[i] = leaves.get(idxs[i]);
     }
     return result;
 }
